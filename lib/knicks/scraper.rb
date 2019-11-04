9421e8971897f3a31e68e96319fbfd922227d105
Brian Pompey <brianthescholar@gmail.com>
2019-11-04 03:03:13 +0000

    scraping

--- lib/knicks/scraper.rb
+++ lib/knicks/scraper.rb
@@ -1,10 +1,17 @@
+require 'open-uri'
+require 'pry'
+
 class Scraper
 
   def get_page
     Nokogiri::HTML(open("https://www.nba.com/knicks/schedule"))
   end
 
-  
+  def scrape_game_index
+    self.get_page.css("div.list-item-date").each do |match|
+      binding.pry
+
+  end
 
 
 
