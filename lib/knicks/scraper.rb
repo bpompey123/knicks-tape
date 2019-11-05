63495c50649ea3eaa22d30876f94e63e84e90f4a
Brian Pompey <brianthescholar@gmail.com>
2019-11-05 01:31:29 +0000

    fix bin

--- lib/knicks/scraper.rb
+++ lib/knicks/scraper.rb
@@ -4,21 +4,30 @@ require 'pry'
 
 class Scraper
 
-  def get_page
-    Nokogiri::HTML(open("https://www.nba.com/knicks/schedule"))
-  end
+  site = "https://www.basketball-reference.com/teams/NYK/2020_games.html"
 
-  def scrape_game_index
-    games = []
-    self.get_page.css("div.season-list").each do |matches|
-      matches.css("div.list-item-date").each do |
+  doc = Nokogiri::HTML(open(site))
 
+  games = doc.css("table.stats_table tbody tr")
 
+  games.each do |match|
+    all_games = []
+    data = match.css("td")
+    data.each do |match_info|
+      stat = match_info.attr("data-stat")
+      case stat
+      when "date_game"
+        date = match_info.text
+      when "game_start_time"
+        time = match_info.text
+      when "opp_name"
+        opponent = match_info.text
 
+      all_games << {date: date, time: time, opponent: opponent}
+      end
 
     end
   end
-
 
 
 
