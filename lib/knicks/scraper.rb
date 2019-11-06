09895c35fc10c7053755a4788ea42d9173302dc0
Brian Pompey <brianthescholar@gmail.com>
2019-11-06 02:40:46 +0000

    changed scraper site

--- lib/knicks/scraper.rb
+++ lib/knicks/scraper.rb
@@ -4,29 +4,27 @@ require 'pry'
 
 class Scraper
 
-  site = "https://www.basketball-reference.com/teams/NYK/2020_games.html"
+  site = "https://basketball.realgm.com/nba/teams/New-York-Knicks/20/Schedule"
 
   doc = Nokogiri::HTML(open(site))
 
-  games = doc.css("table.stats_table tbody tr")
+  games = doc.css("table.basketball tbody tr")
 
   games.each do |match|
     all_games = []
     data = match.css("td")
     data.each do |match_info|
-      stat = match_info.attr("data-stat")
-      case stat
-      when "date_game"
-        date = match_info.text
-      when "game_start_time"
-        time = match_info.text
-      when "opp_name"
-        opponent = match_info.text
+      stat = match_info.attr("data-th")
+      if stat == "Date"
+        date = match_info.css("a").text
+      elsif stat == "Opponent"
+        opponent =  match_info.css("a").text
+      elsif stat == "Venue"
+        location = match_info.css("a").text
 
-      all_games << {date: date, time: time, opponent: opponent}
+      all_games << {date: date, opponent: opponent, location: location}
       end
     end
-    all_games
   end
 
 
