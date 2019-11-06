8b43ffe98e5c5ac95a8af208a0acc71c7a0a36ef
Brian Pompey <brianthescholar@gmail.com>
2019-11-06 03:04:02 +0000

    working...

--- lib/knicks/scraper.rb
+++ lib/knicks/scraper.rb
@@ -10,22 +10,34 @@ class Scraper
 
   games = doc.css("table.basketball tbody tr")
 
-  games.each do |match|
-    all_games = []
-    data = match.css("td")
-    data.each do |match_info|
-      stat = match_info.attr("data-th")
-      if stat == "Date"
-        date = match_info.css("a").text
-      elsif stat == "Opponent"
-        opponent =  match_info.css("a").text
-      elsif stat == "Venue"
-        location = match_info.css("a").text
+  def list_all_games
+    games.each do |match|
+      all_games = []
+      data = match.css("td")
+      data.each do |match_info|
+        stat = match_info.attr("data-th")
+        if stat == "Date"
+          date = match_info.css("a").text
+        elsif stat == "Opponent"
+          opponent =  match_info.css("a").text
+        elsif stat == "Venue"
+          location = match_info.css("a").text
 
-      all_games << {date: date, opponent: opponent, location: location}
+        all_games << {date: date, opponent: opponent, location: location}
+        end
       end
+      all_games
     end
   end
+
+
+  def game_finder
+    all_games.find { ||}
+
+
+  end
+
+
 
 
 
