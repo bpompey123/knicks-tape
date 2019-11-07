77d341174596adf4087626dad2c28f2d520e44d3
Brian Pompey <brianthescholar@gmail.com>
2019-11-07 18:34:46 +0000

    edit games

--- lib/knicks/games.rb
+++ lib/knicks/games.rb
@@ -3,25 +3,33 @@ require_relative "./scraper.rb"
 class Games
   attr_accessor :date, :location, :opponent
 
-#  @@all = []
+  @@all = []
 
-#  def initialize
-#    @@all << self
-#  end
+  def initialize(date, location, opponent)
+    @date= date
+    @location= location
+    @opponent= opponent
+    @@all << self
+  end
 
-#  def self.all
-#    @@all
-#  end
-  def find_by_date
+  def self.all
+    @@all
+  end
+
+  def game
 
   end
 
-  def find_by_opponent
-
+  def find_by_date(date)
+    game.find { |match| match.date == date}
   end
 
-  def find_by_location
+  def find_by_opponent(opponent)
+    game.find { |match| match.opponent == opponent}
+  end
 
+  def find_by_location(location)
+    game.find { |match| match.location == location}
   end
 
 
